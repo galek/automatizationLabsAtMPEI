808f5c2fba645265e86e45750892b557be77cfea
Nick Galko <nikolay.galko@gmail.com>
2017-06-06 14:20:57 +0400

    Расширил список моделей

---
+++ AvtoExportClient/VFTS_750.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoExportClient
+{
+    public class VFTS_750 : VFTS_Base
+    {
+        public VFTS_750()
+        {
+            Name = "ВФТС 750 кг";
+            Weight = 750;
+            WithCompositeMaterials = true;
+
+            СтабилизаторПоперечнойУчтойчивостиИМатериал = new Tuple<bool, _МатериалыМеталл>(true, _МатериалыМеталл.Титан);
+            МассаМаховикаИМатериал = new Tuple<float, _МатериалыМеталл>((float)3.2, _МатериалыМеталл.Титан); // Заводской вес сильно больше
+            ПрямозубаяКоробкаПередач = true;
+            КоличествоСтупеней = 5;
+            Price = 40000;
+            СтепеньСжатия = (float)12.5;
+            МодельИспользуемогоРаспредвала = "VFTS-3";
+        }
+    }
+}
---
+++ AvtoExportClient/VFTS_850.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoExportClient
+{
+    public class VFTS_850 : VFTS_Base
+    {
+        public VFTS_850()
+        {
+            Name = "ВФТС 850 кг";
+            Weight = 850;
+            СтабилизаторПоперечнойУчтойчивостиИМатериал = new Tuple<bool, _МатериалыМеталл>(true, _МатериалыМеталл.Титан);
+            МассаМаховикаИМатериал = new Tuple<float, _МатериалыМеталл>((float)4.0, _МатериалыМеталл.УсиленнаяСталь); // Заводской вес сильно больше
+            ПрямозубаяКоробкаПередач = true;
+            КоличествоСтупеней = 5;
+            Price = 25000;
+            МодельИспользуемогоРаспредвала = "VFTS-2";
+        }
+    }
+}

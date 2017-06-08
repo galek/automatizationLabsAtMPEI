e0e0ac6f394b3a4a9aca1bdfa6a2312e891c1fb0
Nick Galko <nikolay.galko@gmail.com>
2017-06-08 13:42:43 +0300

    Немного другие размеры

---
+++ AvtoExportClient/COMClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtoExportClient
+{
+    //https://msdn.microsoft.com/en-us/library/aa645736(v=vs.71).aspx
+    class COMClient
+    {
+    }
+}
